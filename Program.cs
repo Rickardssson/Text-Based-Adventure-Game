50c0d00aeec1ab24458041cb0e6a963055beb06b
Tore Forsberg <erotfor@gmail.com>
2024-09-08 16:00:48 +0200

    Add an introduction where player name is assigned
    Co-Authored-By: Rickardssson <180270630+Rickardssson@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("You find yourself in a bar and see the bartender aproach you.");
+        string name = "";
+
+        do
+        {
+            name = Ask("What's your name kid? ");
+        } while (!AskYesOrNo($"{name} is it? "));
+    }
+
+    static string Ask(string question)
+    {
+        string response = "";
+        while (response == "")
+        {
+            Console.Write(question);
+            response = Console.ReadLine().Trim();
+        }
+
+        return response;
+    }
+
+    static bool AskYesOrNo(string question)
+    {
+        while (true)
+        {
+            string response = Ask(question).ToLower();
+            switch (response)
+            {
+                case "yes":
+                    case "ok":
+                    return true;
+                case "no":
+                    return false;
+            }
+        }
+    }
+        
+}
