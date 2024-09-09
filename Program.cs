15b8e5eb7c82ef8f97628f6300c7acbce98b0849
Tore Forsberg <erotfor@gmail.com>
2024-09-09 12:57:01 +0200

    Update program and add starting location
    Co-Authored-By: Rickardssson <180270630+Rickardssson@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -8,19 +8,26 @@
             if (player.Location == "newgame")
             {
                 NewGame(player);
+            }
 
-            } else
+            if (player.Location == "bar")
+            {
+                Bar(player);
+            }
+            else
             {
                 Console.Error.WriteLine($"You forgot to implement '{player.Location}'!");
             }
         }
-        Console.WriteLine("You find yourself in a bar and see the bartender aproach you.");
+        
     }
     
     class Player
     {
         public string Name = "";
+        public string FinalOpponentName = "";
         public int Popularity = 0;
+        public int Courage = 50;
         public List<string> Items = new List<string>();
         public string Location = "newgame";
     }
@@ -28,14 +35,61 @@
     static void NewGame(Player player)
     {
         Console.Clear();
+        Console.WriteLine("You find yourself in a bar and see the bartender aproach you.");
         string name = "";
         do
         {
             name = Ask("What's your name kid? ");
         } while (!AskYesOrNo($"{name} is it? "));
 
+        int initialPopularity = 0;
+        do
+        {
+            try
+            {
+                initialPopularity = int.Parse(Ask("How many friends did you come with? "));
+                if (initialPopularity == 0)
+                {
+                    //
+                    initialPopularity = 1;
+                }
+            }
+            catch (Exception e)
+            {
+                continue;
+            }
+        } while (initialPopularity == 0);
+
         player.Name = name;
         player.Location = "bar";
+        player.Popularity = initialPopularity;
+    }
+
+    static void Bar(Player player)
+    {
+        if (player.FinalOpponentName == "")
+        {
+            Console.Write($"Well {player.Name} pick your poison? ");
+            player.FinalOpponentName = "Mr. " + Console.ReadLine();
+            Console.WriteLine("We don't have that so have this instead.");
+        }
+
+        Console.WriteLine("\nThe bartender hands you a shot of some indistinguishable liquid.");
+                
+        if (AskYesOrNo("Do you want to drink the shot? "))
+        {
+            player.Courage += 5;
+            Console.WriteLine("You gained 5 Courage and an empty shot glass!")
+            player.Items.Add("Empty Shot Glass");
+        } else
+        {
+            Console.WriteLine("You gained a shot!")
+            player.Items.Add("Shot");
+        }
+        
+        // To stop program from spamming text
+        Console.WriteLine("Where do you go next? ");
+        Console.ReadLine();
     }
 
     static string Ask(string question)
