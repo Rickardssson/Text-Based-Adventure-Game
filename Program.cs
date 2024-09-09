3c7568c3f28b9c13cbac9cef5726c849e202745a
Tore Forsberg <erotfor@gmail.com>
2024-09-09 12:23:41 +0200

    Implement a player class and update code structure
    Also error message for non existing environments
    
    Co-Authored-By: Rickardssson <180270630+Rickardssson@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -2,13 +2,40 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("You find yourself in a bar and see the bartender aproach you.");
-        string name = "";
+        Player player = new Player();
+        while (player.Location != "quit")
+        {
+            if (player.Location == "newgame")
+            {
+                NewGame(player);
 
+            } else
+            {
+                Console.Error.WriteLine($"You forgot to implement '{player.Location}'!");
+            }
+        }
+        Console.WriteLine("You find yourself in a bar and see the bartender aproach you.");
+    }
+    
+    class Player
+    {
+        public string Name = "";
+        public int Popularity = 0;
+        public List<string> Items = new List<string>();
+        public string Location = "newgame";
+    }
+
+    static void NewGame(Player player)
+    {
+        Console.Clear();
+        string name = "";
         do
         {
             name = Ask("What's your name kid? ");
         } while (!AskYesOrNo($"{name} is it? "));
+
+        player.Name = name;
+        player.Location = "bar";
     }
 
     static string Ask(string question)
