4616d939a7fbf696e5166b25579bd5d3186b6e32
Tore Forsberg <erotfor@gmail.com>
2024-09-09 16:15:14 +0200

    Add bathroom and refactor code for taking shot
    Co-Authored-By: Rickardssson <180270630+Rickardssson@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -14,6 +14,11 @@
             {
                 Bar(player);
             }
+
+            if (player.Location == "bathroom")
+            {
+                Bathroom(player);
+            }
             else
             {
                 Console.Error.WriteLine($"You forgot to implement '{player.Location}'!");
@@ -64,21 +69,75 @@
         }
 
         Console.WriteLine("The bartender hands you a shot of some indistinguishable liquid.");
-                
+
+        ObtainShot(player);
+        
+        // Taget från https://stackoverflow.com/a/723219
+        List<string> availableLocations = new List<string> { "bathroom", "dancefloor" };
+        
+        player.Location = GetListValue("Where do you go next? Bathroom or Dancefloor? ", availableLocations);
+    }
+
+    static void Bathroom(Player player)
+    {
+        Console.WriteLine("You enter the bathroom.");
+        if (player.Items.Contains("Jacket") || player.Items.Contains("Wardrobe Ticket"))
+        {
+            Console.WriteLine("Nothing happens");
+        }
+        else
+        {
+            Console.WriteLine("A shady figure approaches from one of the stalls.");
+            List<string> options = new List<string> { "shot", "ticket" };
+            
+            string choice = GetListValue("Shot or a ticket, the choice is yours... ", options);
+            if (choice == "shot")
+            {
+                ObtainShot(player);
+            } else
+            {
+                player.Items.Add("Wardrobe Ticket");
+                Console.WriteLine("You have obtained a ticket.");
+                Console.WriteLine("What is it for?");
+            }
+
+        }
+
+        List<string> availableLocations = new List<string> { "bar", "wardrobe" };
+        
+        player.Location = GetListValue("Where do you go next? Bar or Wardrobe? ", availableLocations);
+    }
+
+    
+    
+    static void ObtainShot(Player player)
+    {
         if (AskYesOrNo("Do you want to drink the shot? "))
         {
-            player.Courage += 5;
-            player.Items.Add("Empty Shot Glass");
-            Console.WriteLine("You gained 5 Courage and an empty shot glass!");
+            DrinkShot(player);
         } else
         {
             player.Items.Add("Shot");
             Console.WriteLine("You gained a shot!");
         }
-        
-        // To stop program from spamming text
-        Console.WriteLine("Where do you go next? ");
-        Console.ReadLine();
+    }
+
+    static void DrinkShot(Player player)
+    {
+        player.Courage += 5;
+        player.Items.Add("Empty Shot Glass");
+        Console.WriteLine("You gained 5 Courage and an empty shot glass!");
+    }
+    
+    static string GetListValue(string question, List<string> list)
+    {
+        string returnValue = "";
+        do
+        {
+            returnValue = Ask(question).ToLower();
+        } while(!list.Contains(returnValue));
+
+        return returnValue;
     }
 
     static string Ask(string question)
