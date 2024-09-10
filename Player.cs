90aefbb0ab6b4e997883083d72c5733e542fe9df
Tore Forsberg <erotfor@gmail.com>
2024-09-10 10:19:58 +0200

    Move Player class to separate file
    Co-Authored-By: Rickardssson <180270630+Rickardssson@users.noreply.github.com>

---
+++ Player.cs
@@ -0,0 +1,9 @@
+﻿class Player
+{
+    public string Name = "";
+    public string FinalOpponentName = "";
+    public int Popularity = 0;
+    public int Courage = 50;
+    public List<string> Items = new List<string>();
+    public string Location = "newgame";
+}
--- Program.cs
+++ Program.cs
@@ -28,16 +28,6 @@
         }
         
     }
-    
-    class Player
-    {
-        public string Name = "";
-        public string FinalOpponentName = "";
-        public int Popularity = 0;
-        public int Courage = 50;
-        public List<string> Items = new List<string>();
-        public string Location = "newgame";
-    }
 
     static void NewGame(Player player)
     {
