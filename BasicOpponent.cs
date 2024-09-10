fe4f38d11d39579738798ee4ad89c6ca984d7ca4
Tore Forsberg <erotfor@gmail.com>
2024-09-10 11:31:18 +0200

    Create a BasicOpponent class
    Co-Authored-By: Rickardssson <180270630+Rickardssson@users.noreply.github.com>

---
+++ BasicOpponent.cs
@@ -0,0 +1,41 @@
+﻿class BasicOpponent : Opponent
+{
+    private Random random = new Random();
+    public BasicOpponent(string name, int popularity, int popularityDifference) : base(name, popularity, popularityDifference)
+    {
+        Name = name;
+        Popularity = popularity;
+        PopularityDifference = popularityDifference;
+    }
+    public void Action1()
+    {
+        Console.WriteLine($"{this.Name} used 'TikTok Dance'!");
+        if (this.random.Next(2) == 1)
+        {
+            this.Popularity += 2;
+            Console.WriteLine($"{this.Name} danced well and 2 people joined their crowd.");
+        }
+        else
+        {
+            this.Popularity -= 2;
+            Console.WriteLine($"{this.Name} danced poorly and 2 people left their crowd.");
+        }
+    }
+
+    public void Action2(Player player)
+    {
+        Console.WriteLine($"{this.Name} used 'Roast Opponent'!");
+
+        string[] roasts =
+        {
+            "Are you dancing? Looks more like tripping over air.",
+            "Elegance isn't your dancefloor strong suit.",
+            "You dance like a malfunctioning robot.",
+        };
+        
+        Console.WriteLine($"{roasts[this.random.Next(roasts.Length)]}");
+        this.Popularity += 1;
+        player.Popularity -= 1;
+        Console.WriteLine($"One member of {player.Name}'s crowd joins the crowd of {this.Name}.");
+    }
+}
