a78f595c75572ac614012b0f1f2eee4ae933502d
Tore Forsberg <erotfor@gmail.com>
2024-09-10 10:56:15 +0200

    Add opponent class
    Co-Authored-By: Rickardssson <180270630+Rickardssson@users.noreply.github.com>

---
+++ Opponent.cs
@@ -0,0 +1,13 @@
+﻿class Opponent
+{
+    public string Name = "";
+    public int Popularity = 0;
+    public int PopularityDifference = 10;
+
+    public Opponent(string name, int popularity, int popularityDifference)
+    {
+        Name = name;
+        Popularity = popularity;
+        PopularityDifference = popularityDifference;
+    }
+}
